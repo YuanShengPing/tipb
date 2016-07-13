4c3fa4220b9a09c7ec6983a2f4e3614889b8da4b
yuansp <yuansp@tripb.cn>
2016-07-13 14:07:40 +0800

    新建项目

---
+++ testB/testB/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testB
+//
+//  Created by 云开互联 on 16/7/13.
+//  Copyright © 2016年 YunKai HuLian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ testB/testB/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  testB
+//
+//  Created by 云开互联 on 16/7/13.
+//  Copyright © 2016年 YunKai HuLian. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
